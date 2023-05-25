884e237ce2bfa8d8d11bf9fd9a498ad0d1427869
newoong <eric981218@naver.com>
2023-05-25 22:08:10 +0900

    fix : UserCollection 수정

--- RIS/Entity/UserCollection.cpp
+++ RIS/Entity/UserCollection.cpp
@@ -3,21 +3,6 @@
 #include "CompanyUser.h"
 #include "types.h"
 
-
-class UserCollection
-{
-private:
-  list<User*> users;
-public:
-  void signUpNormalUser(string inputName, string inputRegistrationNumber, string inputId, string inputPassword);
-  void signUpCompanylUser(string inputCompanyName, string inputBusinessNumber, string inputId, string inputPassword);
-  void deleteAccount();
-  User *findByBusinessNumber(string businessNumber);
-  User *findByCompanyName(string companyName);
-  User *findByCredential(string id, string password);
-};
-
-
 void UserCollection::signUpNormalUser(string inputName, string inputRegistrationNumber, string inputId, string inputPassword){
     NormalUser *nUser = new NormalUser(inputName, inputRegistrationNumber, inputId, inputPassword, NORMAL_USER);
     users.push_back(nUser);
@@ -26,4 +11,41 @@ void UserCollection::signUpNormalUser(string inputName, string inputRegistration
 void UserCollection::signUpCompanylUser(string inputCompanyName, string inputBusinessNumber, string inputId, string inputPassword){
     CompanyUser *cUser = new CompanyUser(inputCompanyName, inputBusinessNumber, inputId, inputPassword, NORMAL_USER);
     users.push_back(cUser);
+}
+
+void UserCollection::deleteAccount(User *me){
+    int count = (this->users).size();
+    for(auto it = this->users.begin(); it != this->users.end(); it++)
+    {
+        if((*it) == me)
+            this->users.erase(it);
+    }
+}
+
+User *UserCollection::findByBusinessNumber(string businessNumber){
+    int count = (this->users).size();
+    for(auto it = this->users.begin(); it != this->users.end(); it++)
+    {
+        if(((CompanyUser*)(*it))->getBusinessNumber() == businessNumber)
+            return *it;
+    }
+}
+
+User *UserCollection::findByCompanyName(string companyName){
+    int count = (this->users).size();
+    for(auto it = this->users.begin(); it != this->users.end(); it++)
+    {
+        if(((CompanyUser*)(*it))->getCompanyName() == companyName)
+            return *it;
+    }
+}
+
+User *UserCollection::findByCredential(string id, string password){
+    int count = (this->users).size();
+    for(auto it = this->users.begin(); it != this->users.end(); it++)
+    {
+        if(((CompanyUser*)(*it))->getId() == id)
+            if (((CompanyUser*)(*it))->getPassword() == password)
+                return *it;
+    }
 }
