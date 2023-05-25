df0851bc4513b9ac7702f674d097ee745fc6bd51
zockq1 <zockq1@gmail.com>
2023-05-25 16:28:49 +0900

    feat: RecruitmentCollection 헤더 작성

---
+++ RIS/Entity/RecruitmentCollection.h
@@ -0,0 +1,14 @@
+#include "Recruitment.h"
+#include <list>
+using namespace std;
+
+class RecruitmentCollection
+{
+private:
+  list<Recruitment> ownedRecruitment;
+public:
+  RecruitmentCollection();
+  list<Recruitment*> getOwnedRecruitment();
+  void addRecruitment(string companyName, string job, int numberOgHires, string deadline, string businessNumber);
+};
+
