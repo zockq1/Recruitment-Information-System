e8f45a33486c182b85272ff3e8ae6eda0418bf6f
Jang <imsgchang@naver.com>
2023-05-25 19:59:48 +0900

    feat : Login Complete

---
+++ RIS/Authentication/Login.h
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <string>
+using namespace std;
+#pragma once
+
+class Login {
+    public:
+    void loginSubmit(string inputId, string inputPassWord);
+};
--- RIS/Authentication/LoginUI.h
+++ RIS/Authentication/LoginUI.h
@@ -4,3 +4,11 @@
 using namespace std;
 #pragma once
 
+class LoginUI{
+    private:
+        Login *control;
+    public:
+        LoginUI(Login *control);
+        void loginSubmit(ifstream *fin, ofstream *fout);
+
+};
