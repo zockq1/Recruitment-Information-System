e7d16024edaa36fe79b66bc62caf3c0dba039798
zockq1 <zockq1@gmail.com>
2023-05-25 13:51:35 +0900

    feat: UserCollection 헤더 작성

---
+++ RIS/Entity/UserCollection.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <list>
+#include "User.h"
+using namespace std;
+
+class UserCollection
+{
+private:
+  list<User*> users;
+public:
+  void signUpNormalUser(string inputName, string inputRegistrationNumber, string inputId, string inputPassword);
+  void signUpCompanylUser(string inputCompanyName, string inputBusinessNumber, string inputId, string inputPassword);
+  void deleteAccount();
+  User findByBusinessNumber(string businessNumber);
+  User findByCompanyName(string companyName)
+};
